0fa1f8e46002767a3cbeefd4c8d954470d991c4b
shamssharafi <98581394+shamssharafi@users.noreply.github.com>
2022-02-08 00:23:33 +0600

    Car game

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,8 +18,17 @@
                   <h4>  {{ __('You are logged in!') }}</h4>
                     <br/>
                     <h2>Car Brand Finder Game</h2>
-    <a href="#" class="btn btn-info" role="button">Game rules modal</a>
-    <a href="#" class="btn btn-info" role="button">Start the game</a>
+   <!-- <a href="http://localhost/laravel3/blog/resources/views/popup.php" class="btn btn-info" role="button">Game rules modal</a>!-->
+    <button onclick="myFunction()" class="btn btn-info" role="button">Game rules modal</button>
+
+    <p id="popup"></p>
+    
+    <script>
+    function myFunction() {
+      document.getElementById("popup").innerHTML = "<br/>After the start of the game, the user will have 59 seconds to figure out which random car brand the system  has selected. When the user finds a match, the app raises the winner modal. In the winner modal, there are two buttons. Start Again and Save My Score.";
+    }
+    </script>
+    <a href="http://localhost/laravel3/blog/resources/views/auth/countdown.blade.php" class="btn btn-info" role="button">Start the game</a>
                 </div>
             </div>
         </div>
@@ -28,7 +37,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
   </head>
   <body>
   
